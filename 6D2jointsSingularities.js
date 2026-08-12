603ba679cb235b2d24f7cb5eb3fd4bd18b51fef4
Ifig Le Gonidec <ifig.le-gonidec@etu.univ-nantes.fr>
2026-08-12 14:14:49 +0200

    6D tries

--- 6D2jointsSingularities.js
+++ 6D2jointsSingularities.js
@@ -1,17 +1,27 @@
-
 Algebra(6, 0, 0, () => {
+  const pgaToG6 = (c01, c02, c03, c12, c13, c23) => {
+    return c01 * 1e1 + c02 * 1e2 + c03 * 1e3 + c12 * 1e4 + c13 * 1e5 + c23 * 1e6;
+  };
 
+  const L1_skew = pgaToG6(1, 0, 0, 0, 1, 0);
+  const L2_skew = pgaToG6(0, 1, 0, -1, 0, 0);
+  
+  const wedge_skew = L1_skew ^ L2_skew;
+  const is_skew_singular = wedge_skew.Length === 0;
 
-    var L1 = 1e12 + 2e13 + 3e14; 
-    var L2 = 1e12 + 2e13 + 4e14; 
+  const L1_coax = pgaToG6(1, 2, 3, 4, 5, 6);
+  const L2_coax = pgaToG6(2, 4, 6, 8, 10, 12);
+  
+  const wedge_coax = L1_coax ^ L2_coax;
+  const is_coax_singular = wedge_coax.Length === 0;
 
-    var meet = L1 ^ L2;
-    console.log("Produit extérieur (L1 ^ L2) :", meet);
-    console.log("Norme du produit :", meet.Length);
-    
-    if (meet.Length < 0.0001) {
-        console.log("Singularité détectée : Les lignes sont intersectantes ou coaxiales.");
-    } else {
-        console.log("Pas de singularité : Les lignes sont skew (non-intersectantes).");
-    }
+  const L1 = pgaToG6(1, 0, 0, 0, 0, 0);
+  const L2 = pgaToG6(0, 1, 0, 0, 0, 0);
+  const L3 = pgaToG6(0, 0, 1, 0, 0, 0);
+  const L4 = pgaToG6(0, 0, 0, 1, 0, 0);
+  const L5 = pgaToG6(0, 0, 0, 0, 1, 0);
+  const L6 = pgaToG6(0, 0, 0, 0, 0, 1);
+
+  const full_robot_singularity = L1 ^ L2 ^ L3 ^ L4 ^ L5 ^ L6;
+  const is_full_robot_singular = full_robot_singularity.Length === 0;
 });
